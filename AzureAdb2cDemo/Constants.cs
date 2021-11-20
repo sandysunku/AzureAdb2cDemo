0ff95fab149ce397d7722a643cad373b45275134
sandysunku <sandeep.sunku@siemens.com>
2021-11-20 19:55:56 +0530

    updated constants data

--- AzureAdb2cDemo/Constants.cs
+++ AzureAdb2cDemo/Constants.cs
@@ -2,13 +2,14 @@
 {
     internal static class Constants
     {
-        internal static readonly string TenantName = "sandysruthib2c";
+        // Fill all the required details here after creating azure adb2c tenant
+        internal static readonly string TenantName = "";
 
-        internal static readonly string TenantId = "d5eb2724-ab28-4a30-998e-de8b5a6f8744";
+        internal static readonly string TenantId = "";
 
-        internal static readonly string ClientId = "08813d26-3308-4fd6-a941-cb5b04d68c21";
+        internal static readonly string ClientId = "";
 
-        internal static readonly string PolicySignin = "b2c_1_sandyflow";
+        internal static readonly string PolicySignin = "";
 
         internal static readonly string[] Scopes = { "" };
 
@@ -16,6 +17,6 @@
 
         internal static string AuthoritySignin => $"{AuthorityBase}{PolicySignin}";
 
-        internal static string RedirectUri = $"msauth://com.sandy.azureadauthenticationdemo/in%2FAfV20SjPwJmYPjMdaTzdpe68%3D";
+        internal static string RedirectUri = "";
     }
 }
